004110aa61d4eb0bdd393a15eddee22e556e36d6
Diana95 <dm950607@gmail.com>
2025-01-28 19:33:31 -0500

    primeros pasos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let amigosAleatorios = 10;
+
+function asignarTextoElemento (elemento,texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function agregarAmigo(){
+    let amigoSecreto = document.getElementById('amigo').value;
+    document.getElementById('añadir');
+    console.log(amigoSecreto);
+}
