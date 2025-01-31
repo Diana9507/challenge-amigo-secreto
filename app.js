9d700532066f997899cf300f2d1a117caea577ff
Diana95 <dm950607@gmail.com>
2025-01-30 19:23:23 -0500

    generando loop

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
-let amigosAleatorios = 10;
+// let amigosAleatorios = 10;
 
-function asignarTextoElemento (elemento,texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
+// function asignarTextoElemento (elemento,texto){
+   // let elementoHTML = document.querySelector(elemento);
+   // elementoHTML.innerHTML = texto;
+// }
+
+    function agregarAmigo(){
+        let amigoSecreto = document.getElementById('amigo').value;
+        document.getElementById('añadir');
+        console.log(amigoSecreto);
+        console.log(amigos)
+
+        if(amigoSecreto == 'ana'){
+            alert('acertaste');
+        }else{
+            if(amigos.includes(amigoSecreto)){
+                return agregarAmigo();
+            }else{
+                amigos.push(amigoSecreto);
+                return amigoSecreto;
+            }
+        }
 }
 
-function agregarAmigo(){
-    let amigoSecreto = document.getElementById('amigo').value;
-    document.getElementById('añadir');
-    console.log(amigoSecreto);
-}
