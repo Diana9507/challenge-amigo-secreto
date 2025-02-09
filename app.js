f18be2afa347fc0e152577f19c6b56fe564d5584
Diana95 <dm950607@gmail.com>
2025-02-09 14:43:39 -0500

    version 2.0

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-        /*function asignarTextoElemento (elemento,texto){
-        let elementoHTML = document.querySelector(elemento);
-        elementoHTML.innerHTML = texto; 
-} */
 
      function agregarAmigo(){
-        let amigoSecreto = document.getElementById('amigo').value;
+        let amigoSecreto = document.getElementById('amigo').value.trim();
         document.getElementById('añadir');
-        
-                
+               
         console.log(amigoSecreto);
         console.log(amigos);
     
-        if(amigoSecreto == 0){
+        if(amigoSecreto.trim() === ''){
             alert('ingresa un nombre');
         }else{if(amigos.includes(amigoSecreto)){
             alert('ingresar otro nombre');
+            return;
         }else{   
                amigos.push(amigoSecreto);
-                let listado = document.createElement('li');
-                listado.textContent = amigoSecreto;
-                let elementoNombreLista = document.querySelector('.name-list');
-                elementoNombreLista.append(listado);
-                // recorrerAmigos();
+                recorrerAmigos();
                 limpiarCaja();
         }
         
@@ -47,15 +39,16 @@ let amigos = [];
         resultadoAmigo.innerHTML= amigos[amigoGenerado];
     }
 
-    // queda en revisión este código
+        function recorrerAmigos(){
 
-    /*function recorrerAmigos(){
-
+        let elementoNombreLista = document.querySelector('.name-list');
+        elementoNombreLista.innerHTML='';
+        
         for(let i = 0;i<amigos.length; i++){
 
         let listado = document.createElement('li');
         listado.textContent = amigos[i];
-        let elementoNombreLista = document.querySelector('.name-list');
-        elementoNombreLista.append(listado);     
+        elementoNombreLista.append(listado);
+             
 }
-}*/
+}
