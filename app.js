218aea850d1524891a25e79c2d31a3096ffdb8b1
Diana95 <dm950607@gmail.com>
2025-02-08 19:35:45 -0500

    version 1

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-
-    function asignarTextoElemento (elemento,texto){
+        /*function asignarTextoElemento (elemento,texto){
         let elementoHTML = document.querySelector(elemento);
-        elementoHTML.innerHTML = texto;
-}
+        elementoHTML.innerHTML = texto; 
+} */
 
-    function agregarAmigo(){
+     function agregarAmigo(){
         let amigoSecreto = document.getElementById('amigo').value;
         document.getElementById('añadir');
         
+                
         console.log(amigoSecreto);
         console.log(amigos);
     
@@ -18,49 +18,44 @@ let amigos = [];
             alert('ingresa un nombre');
         }else{if(amigos.includes(amigoSecreto)){
             alert('ingresar otro nombre');
-            recorrerAmigos();
-            amigoAleatorio();
-            amigos.length=0;
-        }else{
-                amigos.push(amigoSecreto);
+        }else{   
+               amigos.push(amigoSecreto);
+                let listado = document.createElement('li');
+                listado.textContent = amigoSecreto;
+                let elementoNombreLista = document.querySelector('.name-list');
+                elementoNombreLista.append(listado);
+                // recorrerAmigos();
                 limpiarCaja();
-                
         }
-                
+        
      }  
             
-    }    
+    }
 
    function limpiarCaja(){
     document.querySelector('#amigo').value='';
-    } 
+   }
     
-   function recorrerAmigos(){
+   function sortearAmigo(){
 
-        for(let i = 0;i<amigos.length; i++){
-
-                let listado = document.createElement('li');
-                listado.textContent = amigos[i];
-                let elementoNombreLista = document.querySelector('.name-list');
-                elementoNombreLista.append(listado);
-    }
-}
-
-
-    function amigoAleatorio(){
-
+        document.getElementById('sortearAmigo');
         let amigoGenerado = String(Math.floor(Math.random()*amigos.length));
         console.log(amigos);
         console.log(amigoGenerado);
 
         let resultadoAmigo = document.getElementById('resultado');
         resultadoAmigo.innerHTML= amigos[amigoGenerado];
-
     }
 
-    
+    // queda en revisión este código
 
+    /*function recorrerAmigos(){
 
+        for(let i = 0;i<amigos.length; i++){
 
-
-   // asignarTextoElemento('resultado',amigos[amigoGenerado]);
+        let listado = document.createElement('li');
+        listado.textContent = amigos[i];
+        let elementoNombreLista = document.querySelector('.name-list');
+        elementoNombreLista.append(listado);     
+}
+}*/
